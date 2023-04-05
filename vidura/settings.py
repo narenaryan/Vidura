2c5cefe06ffd85305ef6a1d93450c33fefda6780
N3N <narenarya@live.com>
2023-04-05 16:24:38 -0700

    feat: add postgresql database conf for production

--- vidura/settings.py
+++ vidura/settings.py
@@ -80,11 +80,23 @@ WSGI_APPLICATION = 'vidura.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+if os.getenv("ENV") == "production":
+    db_conf = {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': '5432',
+    }
+else:
+    db_conf = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+
+DATABASES = {
+    'default': db_conf
 }
 
 
