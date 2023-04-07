e1a876a4eb96d030e4fc4e25f82dc8af148ad4ad
N3N <narenarya@live.com>
2023-04-07 09:28:14 -0700

    feat: add neon squares logo for Vidura

--- vidura/settings.py
+++ vidura/settings.py
@@ -136,8 +136,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
