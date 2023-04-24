05b9acbd0ddc97198729ade0f0fd4cd1c3788643
Arpith Muddi <arpith.muddi@sap.com>
2023-04-24 11:57:43 +0200

    Load .env file

--- vidura/settings.py
+++ vidura/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
+# load ENV and DJANGO_SETTINGS_MODULE from .env file
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
